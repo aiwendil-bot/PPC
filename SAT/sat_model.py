53c2e50ef04b2da728e8c559aa939c694a26a0a9
Aiwendil <adriencallico@hotmail.fr>
2022-10-03 21:01:26 +0200

    model sat renvoie toujours false

--- SAT/sat_model.py
+++ SAT/sat_model.py
@@ -1,23 +1,25 @@
 from pysat.solvers import Glucose4
+from pysat.formula import CNF
 import numpy as np
 
 
 # modèle simple, avec symétries
 
 def sat_model(q: int, w: int, g: int, p: int):
-    g = Glucose4()
+    model = Glucose4()
 
     Xlist = [i for i in range(1, q * p * g * w + 1)]
 
-    X = np.asarray(Xlist).reshape((q, p, g, w))
+    X = np.asarray(Xlist, int).reshape((q, p, g, w))
 
+    print(X[0,0,0,0].item())
     contrainte1 = []
 
     for i in range(q):
         for l in range(w):
-            contrainte1.append([X[i, j, k, l] for j in range(p) for k in range(g)])
+            contrainte1.append([X[i, j, k, l].item() for j in range(p) for k in range(g)])
 
-    g.append_formula(contrainte1)
+    model.append_formula(contrainte1)
 
     contrainte2 = []
 
@@ -26,9 +28,9 @@ def sat_model(q: int, w: int, g: int, p: int):
             for j in range(p - 1):
                 for k in range(g):
                     for m in range(j + 1, p):
-                        contrainte2.append([-X[i, j, k, l], -X[i, m, k, l]])
+                        contrainte2.append([-X[i, j, k, l].item(), -X[i, m, k, l].item()])
 
-    g.append_formula(contrainte2)
+    model.append_formula(contrainte2)
 
     contrainte3a = []
 
@@ -38,18 +40,18 @@ def sat_model(q: int, w: int, g: int, p: int):
                 for k in range(g - 1):
                     for m in range(k + 1, g):
                         for n in range(p):
-                            contrainte3a.append([-X[i, j, k, l], -X[i, n, m, l]])
+                            contrainte3a.append([-X[i, j, k, l].item(), -X[i, n, m, l].item()])
 
-    g.append_formula(contrainte3a)
+    model.append_formula(contrainte3a)
 
     contrainte3b = []
 
     for l in range(w):
         for k in range(g):
             for j in range(p):
-                contrainte3b.append([X[i, j, k, l] for i in range(q)])
+                contrainte3b.append([X[i, j, k, l].item() for i in range(q)])
 
-    g.append_formula(contrainte3b)
+    model.append_formula(contrainte3b)
 
     contrainte3c = []
 
@@ -58,10 +60,27 @@ def sat_model(q: int, w: int, g: int, p: int):
             for j in range(p):
                 for i in range(q - 1):
                     for m in range(i + 1, q):
-                        contrainte3c.append([-X[i, j, k, l], -X[m, j, k, l]])
+                        contrainte3c.append([-X[i, j, k, l].item(), -X[m, j, k, l].item()])
 
-    g.append_formula(contrainte3c)
+    model.append_formula(contrainte3c)
 
     #reste sociabilisation
 
-sat_model(6, 2, 3, 3)
+    contraintes_socia = []
+
+    for l in range(w-1):
+        for k in range(p):
+            for m in range(q-1):
+                for n in range(m+1,q):
+                    for kk in range(g):
+                        for ll in range(l+1,w):
+                            temp = CNF(from_clauses=[[X[m,j,k,l].item() for j in range(p)], [X[n,j,k,l].item() for j in range(p)],
+                                                     [X[m,j,kk,ll].item() for j in range(p)], [X[n,j,k,l].item() for j in range(p)]])
+                            neg = temp.negate()
+                            model.append_formula(neg.clauses)
+
+    print(model.solve())
+    print(model.get_model())
+
+
+sat_model(12, 4, 3, 2)
